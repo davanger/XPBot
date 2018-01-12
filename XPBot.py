19f9fe4aaaa6b9cbd6343eddd934f2ac4fef1f85
David Bravo <davidr.bravom@gmail.com>
2018-01-12 14:23:21 -0600

    Added a Tiny DB database json for storing player information

--- XPBot.py
+++ XPBot.py
@@ -4,11 +4,17 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import platform
 import configparser
+from tinydb import TinyDB, Query
 
 # Read client secret from config.ini file
 config = configparser.ConfigParser()
 config.read('config.ini')
 discordConfig = config['Discord']
+
+# Set up the database
+
+db = TinyDB("xpBotDB.json")
+playersDB = Query()
 
 # Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
 client = Bot(description="XP bot by Davanger", command_prefix="$", pm_help = True)
@@ -24,37 +30,57 @@ async def on_ready():
 	print('Use this link to invite {}:'.format(client.user.name))
 	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
 
-# Game info
+# Game info functions
 
-players = {}
-
-def addXp(player, xp):
-	if player in players:
-		players[player] = players[player] + xp
+def dbAddXp(playerName, xp):
+	dbPlayer = db.search(playersDB.name == playerName)
+	if len(dbPlayer) == 1:
+		dbXP = dbPlayer[0]['xp']
+		dbXP = dbXP + xp
+		db.update({'xp': dbXP}, playersDB.name == playerName)
+	elif len(dbPlayer) == 0:
+		db.insert({'type': 'player', 'name': playerName, 'xp': xp})
 	else:
-		players[player] = xp
+		print("Duplicate user error")
 
-def clearXp(player):
-	try:
-		players[player] = 0
-	except:
-		print("Player {} doesn't exist!".format(player))
+#def addXp(player, xp):
+#	if player in players:
+#		players[player] = players[player] + xp
+#	else:
+#		players[player] = xp
 
-# Commands
+def dbClearXp(playerName):
+	db.remove(playersDB.name == playerName)
+
+#def clearXp(player):
+#	try:
+#		players[player] = 0
+#	except:
+#		print("Player {} doesn't exist!".format(player))
+
+def dbSetXp(playerName, xp):
+	dbPlayer = db.search(playersDB.name == playerName)
+	if len(dbPlayer) == 1:
+		db.update({'xp': xp}, playersDB.name == playerName)
+	elif len(dbPlayer) == 0:
+		db.insert({'type': 'player', 'name': playerName, 'xp': xp})
+	else:
+		print("Duplicate user error: {}".format(playerName))
+
+# Discord commands
 
 @client.command(pass_context=True)
 async def fail(ctx):
 	await client.say("Failed throw, adding 1 XP to {}.".format(ctx.message.author.mention))
-	addXp(ctx.message.author.mention,1)
+	dbAddXp(ctx.message.author.mention,1)
 	await asyncio.sleep(3)
 		
-
 @client.command(pass_context=True)
 async def gainxp(ctx, *args):
 	if(len(args) == 1):
 		try:
 			await client.say("Adding " + str(int(args[0])) + " XP to {}".format(ctx.message.author.mention))
-			addXp(ctx.message.author.mention,int(args[0]))
+			dbAddXp(ctx.message.author.mention,int(args[0]))
 			await asyncio.sleep(3) 
 		except: #user didn't pass an int
 			await client.say("The XP you're adding must be a number.")
@@ -68,7 +94,7 @@ async def remxp(ctx, *args):
 	if(len(args) == 1):
 		try:
 			await client.say("Removing " + str(int(args[0])) + " XP from {}".format(ctx.message.author.mention))
-			addXp(ctx.message.author.mention,-int(args[0]))
+			dbAddXp(ctx.message.author.mention,-int(args[0]))
 			await asyncio.sleep(3) 
 		except: #user didn't pass an int
 			await client.say("The XP you're removing must be a number.")
@@ -79,10 +105,11 @@ async def remxp(ctx, *args):
 
 @client.command(pass_context=True)
 async def listxp():
-	if len(players) > 0:
+	playersList = db.search(playersDB.type == 'player')
+	if len(playersList) > 0:
 		output = "Current XP table:"
-		for player in players:
-			output = output + "\n" + "{}: {} XP".format(player, str(players[player]))
+		for player in playersList:
+			output = output + "\n" + "{}: {} XP".format(player['name'], str(player['xp']))
 #			await client.say("{}: {} XP".format(player, str(players[player])))
 #			await asyncio.sleep(3)
 		await client.say(output)
@@ -93,9 +120,10 @@ async def listxp():
 
 @client.command(pass_context=True)
 async def resetxp(ctx):
-	if len(players) > 0:
+	playersList = db.search(playersDB.type == 'player')
+	if len(playersList) > 0:
 		await client.say("Resetting XP for {}".format(ctx.message.author.mention))
-		clearXp(ctx.message.author.mention)
+		dbClearXp(ctx.message.author.mention)
 		await asyncio.sleep(3)
 	else:
 		await client.say("There haven't been any registered XP.")
@@ -103,15 +131,17 @@ async def resetxp(ctx):
 
 @client.command(pass_context=True)
 async def resetallplayerxp():
-	if len(players) > 0:
+	playersList = db.search(playersDB.type == 'player')
+	if len(playersList) > 0:
 		output = "Resetting XP table:"
-		for player in players:
-			clearXp(player)
-			output = output + "{}: {} XP".format(player, str(players[player]))
-			await client.say(output)
-			await asyncio.sleep(3)
+		for player in playersList:
+			dbClearXp(player)
+			#output = output + "{}: {} XP".format(player, str(players[player]))
+			#await client.say(output)
+			#await asyncio.sleep(3)
+		listxp() # shouldn't return anything since the player table should be empty by this point
 	else:
-		await client.say("There haven't been any registered XP.")
+		await client.say("There arent any players with registered XP.")
 		await asyncio.sleep(3)
 
 @client.command(pass_context=True)
@@ -119,7 +149,7 @@ async def setxp(ctx, *args):
 	if(len(args) == 1):
 		try:
 			await client.say("Setting {}'s XP to ".format(ctx.message.author.mention) + str(int(args[0])) + " XP.")
-			players[ctx.message.author.mention] = int(args[0])
+			dbSetXp(ctx.message.author.mention, int(args[0]))
 			await asyncio.sleep(3) 
 		except: #user didn't pass an int
 			await client.say("The XP must be a number.")
